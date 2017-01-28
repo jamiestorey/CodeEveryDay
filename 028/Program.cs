55a363767e69315f14752b317e71e575306c743c
jamiestorey <jamie@jamiestorey.com>
2017-01-28 18:59:09 +0000

    C# For Loops

---
+++ 028/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 38; i++)
+        {
+            
+            for (int j = 0; j < i; j++)
+            {
+                
+                Console.Write("*");
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+
+            for (int k = i + 12; k >= i; k-=3)
+            {
+                Console.Write("\t*");
+                for (int l = k; l > k -2; l-=2)
+                {
+                    
+                    Console.Write("[{0}]", l);
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                }
+            }
+            Console.WriteLine();
+            for (int m = 0; m < i - 7; m++)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+        }
+
+        Console.ReadLine();
+    }
+}
